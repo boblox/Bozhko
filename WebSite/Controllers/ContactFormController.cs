b94e1f27ce10755af5c62406a70c8104e1ac7fdd
boblox <boblox12@gmail.com>
2015-02-04 04:41:53 +0200

    Added Contact Form. Added Editor Templates for bootstrap

---
+++ WebSite/Controllers/ContactFormController.cs
@@ -0,0 +1,115 @@
+﻿using Site.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+using Umbraco.Web;
+using umbraco;
+using Site.Helpers;
+using Umbraco.Core.Logging;
+using System.Net.Mail;
+using System.Text;
+using Site.Resources;
+using HtmlAgilityPack;
+
+namespace Site.Controllers
+{
+    public class ContactFormController : SurfaceController
+    {
+        #region Actions
+
+        [HttpPost]
+        public ActionResult Send(ContactForm model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return CurrentUmbracoPage();
+                }
+
+                var body = GetMailBody(model);
+
+                SendEmail(model.EmailFrom,
+                    model.EmailTo,
+                    Localization.ContactFormEmailSubject,
+                    body);
+                TempData.Add("Form received", true);
+            }
+            catch (Exception e)
+            {
+                LogHelper.Error(GetType(), e.ToString(), e);
+            }
+            return RedirectToCurrentUmbracoPage();
+
+        }
+
+        [ChildActionOnly]
+        public ActionResult Index(string emailFrom, string emailTo)
+        {
+            return PartialView("ContactForm", new ContactForm { EmailFrom = emailFrom, EmailTo = emailTo });
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private string GetMailBody(ContactForm model)
+        {
+            var doc = new HtmlDocument();
+            doc.Load(Server.MapPath("~/Email/ContactForm.html"));
+            var builder = new StringBuilder(doc.DocumentNode.SelectSingleNode("//body").WriteTo());
+
+            ReplacePlaceholder(builder, "NameSurnameTitle", Localization.ContactFormNameSurname);
+            ReplacePlaceholder(builder, "NameSurname", model.NameSurname);
+
+            ReplacePlaceholder(builder, "DescriptionTitle", Localization.ContactFormDescription);
+            ReplacePlaceholder(builder, "Description", model.Description);
+
+            ReplacePlaceholder(builder, "HoursTitle", Localization.ContactFormHours);
+            ReplacePlaceholder(builder, "Hours", model.Hours.ToString());
+
+            ReplacePlaceholder(builder, "IsPostProcessingNeededTitle", Localization.ContactFormIsPostProcessingNeeded);
+            ReplacePlaceholder(builder, "IsPostProcessingNeeded", BoolToLocalizedString(model.IsPostProcessingNeeded));
+
+            ReplacePlaceholder(builder, "IsStudioBookingNeededTitle", Localization.ContactFormIsStudioBookingNeeded);
+            ReplacePlaceholder(builder, "IsStudioBookingNeeded", BoolToLocalizedString(model.IsStudioBookingNeeded));
+
+            return builder.ToString();
+        }
+
+        private string BoolToLocalizedString(bool b)
+        {
+            return b ? Localization.True : Localization.False;
+        }
+
+        private void ReplacePlaceholder(StringBuilder builder, string key, string value)
+        {
+            key = string.Format("[{0}]", key);
+            value = HttpUtility.HtmlEncode(value);
+            builder.Replace(key, value);
+        }
+
+        private void SendEmail(string emailFrom, string emailTo, string subject, string body)
+        {
+            var message = new MailMessage
+            {
+                From = new MailAddress(emailFrom, emailFrom),
+                Subject = subject,
+                SubjectEncoding = Encoding.UTF8,
+                Body = body,
+                BodyEncoding = Encoding.UTF8,
+                IsBodyHtml = true
+            };
+            message.To.Add(emailTo);
+
+            var smtpClient = new SmtpClient();
+            smtpClient.Send(message);
+        }
+
+        #endregion
+    }
+}
---
+++ WebSite/Models/ContactForm.cs
@@ -0,0 +1,39 @@
+﻿using Site.Resources;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Core.Models;
+using Umbraco.Web.Models;
+
+namespace Site.Models
+{
+    public class ContactForm
+    {
+        public string EmailFrom { get; set; }
+
+        public string EmailTo { get; set; }
+
+        [Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(Localization))]
+        [Display(Name = "ContactFormNameSurname", ResourceType = typeof(Localization))]
+        public string NameSurname { get; set; }
+
+        [Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(Localization))]
+        [Display(Name = "ContactFormDescription", ResourceType = typeof(Localization))]
+        [AllowHtml]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        //[Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(Localization))]
+        [Display(Name = "ContactFormHours", ResourceType = typeof(Localization))]
+        public int Hours { get; set; }
+
+        [Display(Name = "ContactFormIsPostProcessingNeeded", ResourceType = typeof(Localization))]
+        public bool IsPostProcessingNeeded { get; set; }
+
+        [Display(Name = "ContactFormIsStudioBookingNeeded", ResourceType = typeof(Localization))]
+        public bool IsStudioBookingNeeded { get; set; }
+    }
+}
